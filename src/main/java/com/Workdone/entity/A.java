fb538f9fa073e738ec119f615cb067dded4574aa
S-omnathmisra <103422617+S-omnathmisra@users.noreply.github.com>
2022-08-13 15:05:11 +0530

    Update A.java

--- src/main/java/com/Workdone/entity/A.java
+++ src/main/java/com/Workdone/entity/A.java
@@ -7,6 +7,7 @@ public static void main(String[] args) {
 	System.out.println("Misra");
 	System.out.println("Bishnupur");
 	System.out.println("Bankura");
+	System.out.println("West Bengal");
 }
 
 }
